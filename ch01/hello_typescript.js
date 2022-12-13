5d56af1582243af3d72d3c07945c19f49310b943
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-13 23:30:12 +0000

    Mastering TypeScript Enterprise
    Strong typing

---
+++ ch01/hello_typescript.js
@@ -0,0 +1,4 @@
+"use strict";
+var version = `es6`;
+console.log(`Hello ${version} TypeScript`);
+var myString = `this is a string`;
---
+++ ch01/hello_typescript.ts
@@ -0,0 +1,4 @@
+var version = `es6`;
+console.log(`Hello ${version} TypeScript`);
+
+var  myString: string = `this is a string`;
